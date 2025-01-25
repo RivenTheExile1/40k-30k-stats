9762252fd5a8a41f536ad4bf1153de7665384431
RivenTheExile1 <connor.a.ferwerda@gmail.com>
2025-01-25 13:42:33 -0600

    minor updates

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import xml.etree.ElementTree as ET
 
 # Parse the XML data
@@ -8,17 +7,36 @@ with open('eldar.cat', 'r', encoding='utf-8') as file:
 root = ET.fromstring(xml_data)
 
 # Extract text from all elements in the XML
-
+unit_list = []
 text = ""
 # Loop through the children of the element and get their text
 with open('output.txt', 'w', encoding='utf-8') as file:
     print("working")
     for child in root.iter():
+        # Remove 'typeId' from attributes
+        if 'typeId' in child.attrib:
+            del child.attrib['typeId']
         
-        if child.text == None:
+        if 'id' in child.attrib:
+            del child.attrib['id']
+
+        if 'targetId' in child.attrib:
+            del child.attrib['targetId']
+
+        # Check if the element has type 'unit'
+        if child.attrib.get('type') == 'unit':
+            print("this is a unit")
+            unit_list.append(child.attrib)
+        
+        if child.text is None:
             text = (f'{child.attrib}')
-        if child.text != None and child.attrib != None:
+        elif child.attrib:
             text = (f'{child.attrib} {child.text}')
+        else:
+            text = child.text
         file.write(text)
         file.write('\n')
     print("done working")
+
+    for unit in unit_list:
+        print(unit)
